4bb767c60b167525fddfa3cce30b0be5b1d24170
nydi <stefan.nydegger@gmail.com>
2018-09-21 23:14:07 +0200

    angular (full app) lecture 26

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule} from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule,
-    MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule],
+  exports: [MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule]
 })
 export class MaterialModule {
 }
