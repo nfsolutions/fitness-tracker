d59c0f170dab6b735876381d032e38c7114c93fb
nydi <stefan.nydegger@gmail.com>
2018-09-26 19:13:27 +0200

    angular (full app) lecture 61

--- src/app.routing.module.ts
+++ src/app.routing.module.ts
@@ -4,16 +4,19 @@ import {WelcomeComponent} from './app/welcome/welcome.component';
 import {SignupComponent} from './app/auth/signup/signup.component';
 import {LoginComponent} from './app/auth/login/login.component';
 import {TrainingComponent} from './app/training/training.component';
+import {AuthGuard} from './app/auth/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: WelcomeComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'training', component: TrainingComponent}];
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {
 
