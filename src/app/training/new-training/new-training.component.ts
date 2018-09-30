4120fd6387285f4acd6fb3f47a776c7b0f0b156a
nydi <stefan.nydegger@gmail.com>
2018-09-30 10:14:35 +0200

    angular (full app) lecture 74 working firebase setup

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TrainingService} from '../training.service';
 import {NgForm} from '@angular/forms';
 import {Exercise} from '../exercise.model';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-new-training',
@@ -12,9 +13,13 @@ export class NewTrainingComponent implements OnInit {
 
   exercises: Exercise[];
 
-  constructor(private readonly trainingService: TrainingService) { }
+  constructor(private readonly trainingService: TrainingService,
+              private readonly db: AngularFirestore) { }
 
   ngOnInit() {
+    this.db.collection('availableExercises').valueChanges().subscribe(result => {
+      console.log(result);
+    })
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAX_29g6dCZboS6X6kyxCtwnK-TJsdcZn0',
+    authDomain: 'ng-fitness-tracker-c28ad.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-c28ad.firebaseio.com',
+    projectId: 'ng-fitness-tracker-c28ad',
+    storageBucket: 'ng-fitness-tracker-c28ad.appspot.com',
+    messagingSenderId: '212542394667'
+  }
 };
 
 /*
