6c15f53aa5b39fffef598912a1ee386568e6ed8a
nydi <stefan.nydegger@gmail.com>
2018-10-07 19:26:41 +0200

    angular (full app) lecture 109

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {WelcomeComponent} from './welcome/welcome.component';
-import {AppRoutingModule} from '../app.routing.module';
+import {AppRoutingModule} from './app.routing.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {HeaderComponent} from './navigation/header/header.component';
 import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
-import {WelcomeComponent} from './app/welcome/welcome.component';
-import {AuthGuard} from './app/auth/auth.guard';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {AuthGuard} from './auth/auth.guard';
 import {RouterModule, Routes} from '@angular/router';
 
 // TODO stn load children should be ./training/training.module#TrainingModule, but that doesnt work...
 const routes: Routes = [
   {path: '', component: WelcomeComponent},
-  {path: 'training', loadChildren: 'src/app/training/training.module#TrainingModule', canLoad: [AuthGuard]}
+  {path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard]}
 ];
 
 @NgModule({
