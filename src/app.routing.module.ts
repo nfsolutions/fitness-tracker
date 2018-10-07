c5f189405f0eb3f878dafe779870b326307c014e
nydi <stefan.nydegger@gmail.com>
2018-10-07 17:30:06 +0200

    angular (full app) lecture 107

--- src/app.routing.module.ts
+++ src/app.routing.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {WelcomeComponent} from './app/welcome/welcome.component';
-import {SignupComponent} from './app/auth/signup/signup.component';
-import {LoginComponent} from './app/auth/login/login.component';
 import {TrainingComponent} from './app/training/training.component';
 import {AuthGuard} from './app/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] }
+  {path: '', component: WelcomeComponent},
+  {path: 'training', component: TrainingComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import {LoginComponent} from './login/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {SharedModule} from '../shared/shared.module';
+import {AuthRoutingModule} from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {SharedModule} from '../shared/shared.module';
   imports: [
     ReactiveFormsModule,
     AngularFireAuthModule,
-    SharedModule
+    SharedModule,
+    AuthRoutingModule
     ],
   exports: []
 })
